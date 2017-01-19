0a3d81e60ac683a5b2bec8a26be0f3d69fba2c9f
vsoch <vsochat@stanford.edu>
2017-01-19 09:57:16 -0800

    modified:   setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,13 @@
 sudo apt-get update
 sudo apt-get -y install python-pip
 sudo apt-get -y install nginx
-sudo apt-get -y install daemontools
+sudo apt-get -y install daemontools #supervise
 sudo service nginx start
 git clone https://www.github.com/vsoch/pokemon-server
 cd pokemon-server
 pip install pokemon
 
 myip="$(dig +short myip.opendns.com @resolver1.opendns.com)"
-echo "<h2>telnet ${myip}</h2>" >> index.html
+echo "<h2>telnet ${myip} 5005</h2>" >> index.html
 sudo mv index.html /var/www/index.html
 supervise $HOME/pokemon-server &
